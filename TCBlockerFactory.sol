03f21ffd31e6e771ed413fbf04acf3fbd9c9bede
Ken Preiser <kenpreiser@gmail.com>
2022-08-29 18:21:41 -0700

    Update TCBlockerFactory.sol

--- TCBlockerFactory.sol
+++ TCBlockerFactory.sol
@@ -8,42 +8,34 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./CloneLib.sol";
 
 interface BlockTC {
-    function init(address _newOwner, address factory, address[] calldata blockers) external;
+    function init(address _receiver, address _factory, address[] calldata blockers) external;
+    function transferReceiver(address _newReceiver) external;
+
+    function getReceiver() external view returns (address);
 }
 
 contract TCBlockerFactory is Ownable {
     using EnumerableSet for EnumerableSet.AddressSet;
     address currentImplementation;
-    uint256 public counter;
-
-    mapping(address => uint256) userToIndex;
-    mapping(uint256 => address) deployedImplementations;
 
     EnumerableSet.AddressSet blockedAddresses;
+    EnumerableSet.AddressSet impls;
 
 
-    function nftToDeployedImplementation(address _nft) public view returns (address) {
-        uint c = userToIndex[_nft];
-        //if (c == 0) return address(0); omitted for gas
-        return deployedImplementations[c];
-    }
-    function nftToNotLarvaLabsIndex(address _nft) public view returns (uint) {
-        return userToIndex[_nft];
-    }
-
-    function clone(address _user) public returns (uint256) {
-        require(userToIndex[_user] == 0, "we already have a NLL for this collection");
-        counter++;
-
-        address impl = LibClone.clone(currentImplementation);
-        BlockTC(impl).init(msg.sender, address(this), blockedAddresses.values());
-
-        deployedImplementations[counter] = impl;
-        userToIndex[_user] = counter;
-        return counter;
-    }  
     function getBlockedAddresses() external view returns (address[] memory) {
         return blockedAddresses.values();
+    }
+
+    function cloneForMe() public returns (address) {
+        address impl = LibClone.clone(currentImplementation);
+        BlockTC(impl).init(msg.sender, address(this), blockedAddresses.values());
+        impls.add(impl);
+        return impl;
+    }  
+
+    function transferReceiverFrom(address _newUser, address impl) external {
+        require(BlockTC(impl).getReceiver() == msg.sender, "User not receiver");
+        BlockTC(impl).transferReceiver(_newUser);
     }
 
     function addBlockedAddresses(address[] calldata toBlock) external onlyOwner {
